c7c02c6296d0fc8697a065adf55550aab51e15f1
Dumetella <dumetella@outlook.com>
2021-10-18 03:07:44 +0300

    decided to add sqlite

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import express, { response } from 'express';
 import dotenv from 'dotenv';
 import log4js from 'log4js';
 import cors from 'cors';
+import sqlite3 from 'sqlite3';
 import WeatherProxy from './components/WeatherProxy.js';
 import { locationRequestEnforcer, weatherRequestEnforcer } from './model/InternalRequests.js';
 
 dotenv.config();
 
+//logger section
 const logger = log4js.getLogger();
 
 logger.level = process.env.LOG_LEVEL!;
@@ -15,6 +17,18 @@ if (!process.env.LOG_LEVEL) {
     logger.level = 'info';
 }
 
+//db section
+const db = new sqlite3.Database('./Weather.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        logger.error(err.message);
+    }
+    logger.info('Connected to the weather database.');
+});
+
+//WeatherProxy
+const Weather = new WeatherProxy(process.env.API_KEY!);
+
+//express section
 const app = express();
 const port = process.env.PORT;
 
@@ -23,8 +37,6 @@ app.use(express.json());
 app.use(cors({
     origin: '*',
 }));
-
-const Weather = new WeatherProxy(process.env.API_KEY!);
 
 app.get('/', async (request, response) => {
     response.send('Hi, i am Dumetella');
@@ -57,9 +69,17 @@ const server = app.listen(port, () => console.log(`Running on port ${port}`));
 
 const onProcessSignal = async (signal: NodeJS.Signals) => {
     logger.info('Got signal', signal);
+    db.close((err) => {
+        if (err) {
+            logger.error(err.message);
+        }
+        logger.info('Closed the database connection.');
+    });
     server.close();
     logger.info('Bye...');
 };
 process.on('SIGTERM', onProcessSignal);
 process.on('SIGINT', onProcessSignal);
+
+
 
